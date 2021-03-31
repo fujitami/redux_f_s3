60e791a363759f17ecf1f802d67ea62998b81fc0
fujitami <midori.fujita.0423@gmail.com>
2021-04-01 00:37:59 +0900

    use composeWithDevTools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 // import {
 //   sayHiOnDispatch,
@@ -9,12 +10,18 @@ import { print1, print2, print3 } from './exampleAddons/middleware'
 
 // const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
 
-const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+// const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+
+const composedEnhancer = composedWithDevTools(
+  applyMiddleware(print1, print2, print3)
+)
 
 // const store = createStore(rootReducer, undefined, composedEnhancer)
 
 // Pass enhancer as the second arg, since there's no preloadedState
 // preloadedStateがないため、2番目の引数としてenhancerを渡す
-const store = createStore(rootReducer, middlewareEnhancer)
+// const store = createStore(rootReducer, middlewareEnhancer)
+
+const store = createStore(rootReducer, composedEnhancer)
 
 export default store
